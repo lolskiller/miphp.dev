5a9a94fc5cd0a25fb4d1029f923095bb2ada83cd
Marc Vidal <marc@PHP.(none)>
2014-09-30 15:23:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+        <?php
+        Echo '<p>Hola mundo</p>';
+        if ($a=1 > $b=2) {
+            echo "a es mayor que b";
+        } elseif ($a == $b) {
+            echo "a es igual que b";
+        } else {
+            echo "a es menor que b";
+        }
+
+        phpinfo();
+
+        ?>
+	</body>
+</html>
